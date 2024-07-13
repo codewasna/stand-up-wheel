3b6d8061b5f040ef44291de6b6b2db65f5ea8233
sourabh.gautam <sourabh.gautam@codefire.com>
2024-07-13 17:35:01 +0530

    shuffle the array when start

--- script.js
+++ script.js
@@ -20,6 +20,9 @@ document.addEventListener("DOMContentLoaded", () => {
     "Kundan",
     "Rohit",
   ];
+
+  shuffleArray(names);
+
   let startAngle = 0;
   let spinTimeout = null;
   let spinAngleStart = 10;
@@ -31,6 +34,15 @@ document.addEventListener("DOMContentLoaded", () => {
   startButton.addEventListener("click", spin);
   closePopUp.addEventListener("click", closePopUpHandler);
   removeBtn.addEventListener("click", removeMeHandler);
+
+  function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
 
   function drawWheel() {
     const outsideRadius = 200;
